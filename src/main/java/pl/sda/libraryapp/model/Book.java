34bec3a5cb607bbd6bb93155c2b5b62d0389a944
Klaudia Psikus <klaudia.psikus@gmail.com>
2019-05-07 18:05:21 +0200

    Library model. The structure of the Maven project. Model classes. Builder for the book. Displaying 10 books.

---
+++ src/main/java/pl/sda/libraryapp/model/Book.java
@@ -0,0 +1,51 @@
+package pl.sda.libraryapp.model;
+
+import java.util.Objects;
+
+public class Book {
+
+    private String title;
+    private Author author;
+
+    Book(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
